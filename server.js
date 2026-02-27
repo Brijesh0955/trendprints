5b5ce8eed2f03231b9219f52e81b4ac725ed6f3c
Brijesh <kt5651746@gmail.com>
2026-02-27 22:38:32 +0530

    Clean install with bcryptjs only

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const session = require('express-session');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs'); 
 const path = require('path');
 const fs = require('fs');
 
